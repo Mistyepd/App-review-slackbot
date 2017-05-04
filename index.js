b608d870da86100936afbd674afdd128ced0bfc4
Kate Henderson <kate@paperkite.co.nz>
2017-05-05 11:31:35 +1200

    Add asthma app

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var reviews = require('reviews-to-slack')
-reviews.start({
-  slackHook: 'https://hooks.slack.com/services/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',
-  appId: 'com.bp.bpmenz'
-})
+var apps = [
+{
+	appId:'com.bp.bpmenz',
+	channel:'@kate'
+},
+{
+	appId:'nz.org.asthmafoundation.asthma',
+	channel:'@kate'
+}
+]
+
+for (var i = 0; i < apps.length; i++) {
+  var app = app[i]
+  reviews.start({
+    slackHook: 'https://hooks.slack.com/services/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',
+    appId: app.appId,
+    channel: app.channel,
+    debug: true,
+    interval:30
+  })
+}

74d61439381bfb9fe29a2ed53361132a957dca53
Kate Henderson <kate@paperkite.co.nz>
2017-05-05 11:01:56 +1200

    Initial app commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var reviews = require('reviews-to-slack')
+reviews.start({
+  slackHook: 'https://hooks.slack.com/services/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',
+  appId: 'com.bp.bpmenz'
+})

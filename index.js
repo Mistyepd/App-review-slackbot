a894de88029a076371f931db0a9fba2ffe92cdf5
Kate Henderson <kate@paperkite.co.nz>
2017-05-05 12:08:59 +1200

    Fix typo and add other apps

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 var reviews = require('reviews-to-slack')
 var apps = [
 {
-	appId:'com.bp.bpmenz',
-	channel:'@kate'
+	appId:'com.bp.bpmenz'
 },
 {
-	appId:'nz.org.asthmafoundation.asthma',
-	channel:'@kate'
+	appId:'nz.org.asthmafoundation.asthma'
+},
+{
+	appId:'nz.co.cca.vendswift'
+},
+{
+	appId:'com.bp.bpmeau'
+},
+{
+	appId:'com.firststarcommunications.ampmscratchpower.android'
+},
+{
+	appId:'com.randstad.timesheets'
+},
+{
+	appId:'nz.org.policeassn.nzpa'
 }
 ]
 
 for (var i = 0; i < apps.length; i++) {
-  var app = app[i]
+  var app = apps[i]
   reviews.start({
     slackHook: 'https://hooks.slack.com/services/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',
     appId: app.appId,
-    channel: app.channel,
-    debug: true,
-    interval:30
+    channel: '@kate',
+    debug: false,
+    interval: 60
   })
 }

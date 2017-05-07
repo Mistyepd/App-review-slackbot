c1624aa4ad7041dbe4c814cdda926c50810e4ff5
Kate Henderson <kate@paperkite.co.nz>
2017-05-08 11:34:37 +1200

    Fix indentation

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 var reviews = require('reviews-to-slack')
 var apps = [
-{
-	appId:'com.bp.bpmenz',
-	appName: 'BPMe NZ'
-},
-{
-	appId:'nz.org.asthmafoundation.asthma',
-	appName: 'My Asthma'
-},
-{
-	appId:'nz.co.cca.vendswift',
-	appName: 'VendSwift'
-},
-{
-	appId:'com.bp.bpmeau',
-	appName: 'BPMe AU'
-},
-{
-	appId:'com.firststarcommunications.ampmscratchpower.android',
-	appName: 'AMPM ScratchPower'
-},
-{
-	appId:'com.randstad.timesheets',
-	appName: 'Randstad Timesheets '
-},
-{
-	appId:'nz.org.policeassn.nzpa',
-	appName: 'NZPA'
-}
+	{
+		appId:'com.bp.bpmenz',
+		appName: 'BPMe NZ'
+	},
+	{
+		appId:'nz.org.asthmafoundation.asthma',
+		appName: 'My Asthma'
+	},
+	{
+		appId:'nz.co.cca.vendswift',
+		appName: 'VendSwift'
+	},
+	{
+		appId:'com.bp.bpmeau',
+		appName: 'BPMe AU'
+	},
+	{
+		appId:'com.firststarcommunications.ampmscratchpower.android',
+		appName: 'AMPM ScratchPower'
+	},
+	{
+		appId:'com.randstad.timesheets',
+		appName: 'Randstad Timesheets '
+	},
+	{
+		appId:'nz.org.policeassn.nzpa',
+		appName: 'NZPA'
+	}
 ]
 
 for (var i = 0; i < apps.length; i++) {
-  var app = apps[i]
-  reviews.start({
-    slackHook: 'https://hooks.slack.com/services/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',
-    appId: app.appId,
-    channel: '#app-reviews',
-    debug: false,
-    interval: 60,
-    appName: app.appName
-  })
+	var app = apps[i]
+	reviews.start({
+		slackHook: 'https://hooks.slack.com/services/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',
+		appId: app.appId,
+		channel: '#app-reviews',
+		debug: false,
+		interval: 60,
+		appName: app.appName
+	})
 }
